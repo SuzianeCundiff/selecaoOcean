6fed997f70a0637bb0558e8d408f91dda1583ce9
Suziane Cundiff <suzianecundiff@gmail.com>
2020-07-10 22:45:45 -0400

    Update #01
    Adicionado a maça como forma de aumentar a pontuação.
    0BS: Melhorar o spawn da maçã e animação de coleta.

---
+++ selecaoOcean/Assets/Scripts/Apple.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+    private SpriteRenderer sprtRenderer;
+    private CircleCollider2D circCollider;
+
+    public GameObject collected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprtRenderer = GetComponent<SpriteRenderer>();
+        circCollider = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            sprtRenderer.enabled = false; 
+            circCollider.enabled = false; 
+            collected.SetActive(true);
+
+            GameController.instance.BirdScored();
+
+            Destroy(this.gameObject, 0.2f);
+        }
+    }
+
+
+
+}
---
+++ selecaoOcean/Assets/Scripts/AppleSpawn.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleSpawn : MonoBehaviour
+{
+    public int appleSpawnSize = 7;
+    public GameObject applePrefab;
+    public float spawnRate = 2f;
+    public float columnMin = -2f;
+    public float columnMax = 2f;
+
+    private GameObject[] apples;
+    private Vector2 objectPoolPosition = new Vector2 (-15f, -25f);
+    private float timeSinceLastSpawned;
+    private float spawnXPosition = 10f;
+    private int currentApple = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateApples();
+    }
+
+    void CreateApples(){
+        apples = new GameObject[appleSpawnSize];
+        for (int i = 0; i < appleSpawnSize; i++)
+        {
+            apples[i] = (GameObject) Instantiate(applePrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if(GameController.instance.gameOver == false && timeSinceLastSpawned >= spawnRate){
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+            apples[currentApple].transform.position = new Vector2 (spawnXPosition, spawnYPosition);
+            currentApple++;
+
+            if(currentApple >= appleSpawnSize){
+                CreateApples();
+                currentApple = 0;
+            }
+        }
+    }
+}
