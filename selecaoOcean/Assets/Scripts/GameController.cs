62b6d56f60250cf712195e8f5deb0defab683ce4
Suziane Cundiff <suzianecundiff@gmail.com>
2020-07-10 20:23:50 -0400

    Created
    Base of the project made with the help of the tutorial Making A Flappy Bird Style Game.

---
+++ selecaoOcean/Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public GameObject gameOverText;
+    public Text scoreText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    private int score = 0;
+
+    // Awake is called before Start.
+    void Awake()
+    {
+        if(instance == null){
+            instance = this;
+        } else if (instance != this){
+            Destroy(gameObject); // if this gameController is not needed anymore, it destroys itself.
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver == true && Input.GetMouseButtonDown(0)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdScored(){
+        if(gameOver){
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+    public void BirdDied(){
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+}
